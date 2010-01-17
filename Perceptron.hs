a39d9cbc455585bac33f0461fc9479adc86cd1f0
Larry Diehl <larrytheliquid@gmail.com>
2010-01-17 15:06:11 -0800

    activation function

--- Perceptron.hs
+++ Perceptron.hs
@@ -1,7 +1,10 @@
 module Perceptron where
 
-sumOfProducts :: [Double] -> [Double] -> Double
-sumOfProducts xs ys = sum (zipWith (*) xs ys)
-
 actualOutputDerivative :: Double -> Double
 actualOutputDerivative n = n * (1-) n
+
+sumOfProducts :: [Double] -> [Double] -> Double
+sumOfProducts ns ms = sum (zipWith (*) ns ms)
+
+activate :: Double -> Double
+activate n = 1  / succ (exp (-n))
--- PerceptronTest.hs
+++ PerceptronTest.hs
@@ -3,13 +3,16 @@ import Perceptron
 import Test.HUnit
 import HUnitExtensions
 
-tests = test [
-  "actual-output-derivative given the actual-output returns its derivative" ~: 
+  -- run tests where
+main = runTestTT (TestList [
+                     
+  "actualOutputDerivative given the actualOutput returns its derivative" ~: 
   (0.2494, 0.0001) @~? actualOutputDerivative 0.5250,
   
-  "sum-of-products given two collections returns the sum of the products of each element in the collections" ~:
+  "activate applies the sigmoid function" ~:
+  (0.5250, 0.0001) @~? activate 0.1,
+  
+  "sumOfProducts given two lists returns the sum of the products of each element in the lists" ~:
   (0.1, 0.01) @~? sumOfProducts [1.0, 0.0, 0.0] [0.1, 0.2, -4.0]
-  ]
-
-main :: IO Counts
-main = runTestTT tests
+  
+  ])
