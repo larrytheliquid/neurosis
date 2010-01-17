76cbe5adbe276de9264d801f30e4beae4749d995
Larry Diehl <larrytheliquid@gmail.com>
2010-01-17 13:47:53 -0800

    sum of products

--- Perceptron.hs
+++ Perceptron.hs
@@ -1,4 +1,7 @@
 module Perceptron where
 
+sumOfProducts :: [Double] -> [Double] -> Double
+sumOfProducts xs ys = sum (zipWith (*) xs ys)
+
 actualOutputDerivative :: Double -> Double
 actualOutputDerivative n = n * (1-) n
--- PerceptronTest.hs
+++ PerceptronTest.hs
@@ -3,8 +3,13 @@ import Perceptron
 import Test.HUnit
 import HUnitExtensions
 
-tester = "actual-output-derivative given the actual-output returns its derivative" ~: 
-         (3.0, 0.001) @~? (1.0+2.0)
+tests = test [
+  "actual-output-derivative given the actual-output returns its derivative" ~: 
+  (0.2494, 0.0001) @~? actualOutputDerivative 0.5250,
+  
+  "sum-of-products given two collections returns the sum of the products of each element in the collections" ~:
+  (0.1, 0.01) @~? sumOfProducts [1.0, 0.0, 0.0] [0.1, 0.2, -4.0]
+  ]
 
 main :: IO Counts
-main = runTestTT tester
+main = runTestTT tests
